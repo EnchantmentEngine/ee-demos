9ca04a4b3a0527e8449ae4bd2d24eb90fae5c1d2
Josh Field <10372036+HexaField@users.noreply.github.com>
2025-07-29 08:37:49 +1000

    Initial commit

---
+++ xrengine.config.ts
@@ -0,0 +1,11 @@
+import type { ProjectConfigInterface } from '@ir-engine/projects/ProjectConfigInterface'
+
+const config: ProjectConfigInterface = {
+  onEvent: undefined,
+  thumbnail: '/static/ir-engine_thumbnail.jpg',
+  routes: {},
+  services: undefined,
+  databaseSeed: undefined
+}
+
+export default config
